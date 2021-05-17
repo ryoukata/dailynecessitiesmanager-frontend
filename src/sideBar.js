e471d14a3ba5342f86f153be40a30918657d30d2
ryoukata <ryoya3091@gmail.com>
2021-05-17 17:46:09 +0900

    feat(#10): add Modal Window Component.

--- src/sideBar.js
+++ src/sideBar.js
@@ -11,9 +11,10 @@ export function SideBar() {
     console.log(open);
   });
 
-  // ローカルで試したい場合、index.htmlのbundle.jsを読み込むコードをコメントアウトしないとonClickが動かない。もしくわbundleしてから実行。
+  // ローカルで試したい場合、index.htmlのbundle.jsを読み込むコードをコメントアウトしないとonClickが動かない
   return (
     <> 
+      <p>サイドバー</p>
       <button onClick={changeSideBar}>サイドバー</button>
       <Drawer anchor="left" open={open} width={200} onClose={changeSideBar}>
         <p>サイドバー</p>
