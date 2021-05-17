6d30222ff5579df847327c5b20402fc967408808
ryoukata <ryoya3091@gmail.com>
2021-05-17 16:37:30 +0900

    feat(#8): create component for side-bar.

---
+++ src/sideBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useState } from "react";
+import { Drawer } from "@material-ui/core";
+
+export function SideBar() {
+
+  const [open, setOpen] = useState(false);
+
+  const changeSideBar = (() => {
+    setOpen(!open);
+    console.log(open);
+  });
+
+  // ローカルで試したい場合、index.htmlのbundle.jsを読み込むコードをコメントアウトしないとonClickが動かない。もしくわbundleしてから実行。
+  return (
+    <> 
+      <button onClick={changeSideBar}>サイドバー</button>
+      <Drawer anchor="left" open={open} width={200} onClose={changeSideBar}>
+        <p>サイドバー</p>
+      </Drawer>
+    </>
+  )
+}
